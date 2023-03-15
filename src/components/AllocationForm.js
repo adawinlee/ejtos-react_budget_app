f3419f2708724d8e41ded02a9e7f4ea83cf5fc0b
Ada Lee <adawinlee@gmail.com>
2023-03-14 20:04:17 -0400

    task 2

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -63,12 +63,15 @@ const AllocationForm = (props) => {
                         <option value="Reduce" name="Reduce">Reduce</option>
                     </select>
 
+                    <label className='input-group-prepend' style={{ marginLeft: '2rem' }}>
+                        £
+                    </label>
                     <input
                         required='required'
                         type='number'
                         id='cost'
                         value={cost}
-                        style={{marginLeft: '2rem', size: 10}}
+                        style={{marginLeft: '1rem', size: 10}}
                         onChange={(event)=>setCost(event.target.value)}
                     ></input>
                     <button
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,27 @@
 import React, { useContext } from 'react';
 import { AppContext, appContext } from '../context/AppContext';
 
-const Budget = () => {
-    const { budget } = useContext(AppContext);
+const Budget = (props) => {
+    const { dispatch } = useContext(AppContext);
+    const changeBudget = () => {
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: props.budget,
+        });
+    };
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>
+                Budget: £
+                <input 
+                    type="number" 
+                    step="10" 
+                    max="20000" 
+                    onBlur={event=>changeBudget(event.target.value)}
+                >
+                </input>
+                </span>
         </div>
     );
 };
